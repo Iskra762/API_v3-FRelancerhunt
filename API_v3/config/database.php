8bcfc6cd51d78e0f1a75aeb934fb633870846a17
Iskra762 <123212801+Iskra762@users.noreply.github.com>
2024-03-29 23:19:23 +0000

    Update database.php

--- API_v3/config/database.php
+++ API_v3/config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 // Підключення до бази даних
-define('DB_HOST', '94.130.175.212:3306');
-define('DB_USER', 'krot');
-define('DB_PASS', 'StongPassword123!');
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
 define('DB_NAME', 'API_v3');
 
 // Токен API Frelancehunt
-$token = '0bcec21e3dcaf86804b2ce4b981c5ff89c4f6d56';
+$token = 'your_token';
 
 ?>
